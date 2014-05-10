5b35118d5590969cd242aa3125f8f571e71ef156
Eugene Burmako <xeno.by@gmail.com>
2014-05-10 20:55:18 +0200

    step 9: we're not done yet, because we've accidentally changed the flow of control
        def x = { println("side-effect!"); new Optional(new C) }
        println(x.getOrElse(new C))
    
    Calling getOrElse on a side-effectful prefix is going to double the side-effects,
    because the prefix is used twice in the macro expansion.
    
        [info] Running Test
        side-effect!
        side-effect!
        C

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -2,12 +2,7 @@ class C { override def toString = "C" }
 
 object Test {
   def main(args: Array[String]): Unit = {
-    val x = new Optional(new C)
-    val x1 = x.getOrElse(new C)
-    println(x, x1)
-
-    val y = new Optional(null)
-    val y1 = y.getOrElse({ println("hello"); new C })
-    println(y, y1)
+    def x = { println("side-effect!"); new Optional(new C) }
+    println(x.getOrElse(new C))
   }
 }
