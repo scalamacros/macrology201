ff4cf2e1d8c9556fc623f80274361b7c2ce50c76
Eugene Burmako <xeno.by@gmail.com>
2014-05-10 23:18:49 +0200

    step 19: wat??? a crash in GenICode?!
    Let's change the parameter of the function `foo` to be by-name
    and also introduce a local variable into the argument of a call to `foo`:
    
        def foo(x: => Optional[C]) = x
        println(foo({ val y = new Optional(null); y }).getOrElse(new C))
    
    This simple adjustment to the original test will produce a compiler crash
    in the icode phase, akin to the one reported in https://github.com/scalatest/scalatest/issues/276
    and in countless other macros:
    
    scala.reflect.internal.FatalError:
      symbol value y does not exist in Test.main
         while compiling: /Users/xeno_by/Projects/macrology201/core/src/main/scala/Test.scala
            during phase: icode
         library version: version 2.11.0
        compiler version: version 2.11.0
      reconstructed args: -Ydead-code -bootclasspath /Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/resources.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/rt.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/sunrsasign.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/jsse.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/jce.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/charsets.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/jfr.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/lib/JObjC.jar:/Library/Java/JavaVirtualMachines/jdk1.7.0_40.jdk/Contents/Home/jre/classes:/Users/xeno_by/.ivy2/cache/org.scala-lang/scala-library/jars/scala-library-2.11.0.jar -optimise -Yinline -Yclosure-elim -Yinline-handlers -Yconst-opt -classpath /Users/xeno_by/Projects/macrology201/core/target/scala-2.11/classes:/Users/xeno_by/Projects/macrology201/macros/target/scala-2.11/classes:/Users/xeno_by/.ivy2/cache/org.scala-lang/scala-reflect/jars/scala-reflect-2.11.0.jar
    
      last tree to typer: Ident(y$1)
           tree position: line 6 of /Users/xeno_by/Projects/macrology201/core/src/main/scala/Test.scala
                tree tpe: Null
                  symbol: value y$1
       symbol definition: y$1: Null (a TermSymbol)
          symbol package: <empty>
           symbol owners: value y$1 -> constructor Test$$anonfun$1 -> <$anon: Function0>
               call site: constructor Test$$anonfun$1 in package <empty>
    
    == Source file context for tree position ==
    
         3   def main(args: Array[String]): Unit = {
         4     def foo(x: => Optional[C]) = x
         5     println(foo({ val y = new Optional(null); y }).getOrElse(new C))
         6   }
         7 }
    	at scala.reflect.internal.SymbolTable.abort(SymbolTable.scala:58)
    	at scala.tools.nsc.Global.abort(Global.scala:269)
    	at scala.tools.nsc.backend.icode.GenICode$ICodePhase.genLoadIdent$1(GenICode.scala:883)
    	at scala.tools.nsc.backend.icode.GenICode$ICodePhase.scala$tools$nsc$backend$icode$GenICode$ICodePhase$$genLoad(GenICode.scala:889)
    	at scala.tools.nsc.backend.icode.GenICode$ICodePhase$$anonfun$genLoadArguments$1.apply(GenICode.scala:1131)
    	at scala.tools.nsc.backend.icode.GenICode$ICodePhase$$anonfun$genLoadArguments$1.apply(GenICode.scala:1129)
    	at scala.collection.LinearSeqOptimized$class.foldLeft(LinearSeqOptimized.scala:110)
    	at scala.collection.immutable.List.foldLeft(List.scala:83)
    	at scala.tools.nsc.backend.icode.GenICode$ICodePhase.genLoadArguments(GenICode.scala:1129)
    	at scala.tools.nsc.backend.icode.GenICode$ICodePhase.genLoadApply3$1(GenICode.scala:695)

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -2,7 +2,7 @@ class C { override def toString = "C" }
 
 object Test {
   def main(args: Array[String]): Unit = {
-    def foo(x: Optional[C]) = x
-    println(foo(new Optional(null)).getOrElse(new C))
+    def foo(x: => Optional[C]) = x
+    println(foo({ val y = new Optional(null); y }).getOrElse(new C))
   }
 }
