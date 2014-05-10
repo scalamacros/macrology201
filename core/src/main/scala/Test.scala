a2b2743f1e568b233b073b35fc45ab47c9efff1d
Eugene Burmako <xeno.by@gmail.com>
2014-05-10 22:52:32 +0200

    step 15: refs to externally defined identifiers typically ask for trouble
    In our case, `Optional.ensuringNotNull` is unsafe, because it can be hijacked
    by specially crafted code.
    
        val Optional = "uh-oh"
        println(new Optional(null).getOrElse(null))
    
        [error] /Users/xeno_by/Projects/macrology201/core/src/main/scala/Test.scala:6: value ensuringNotNull is not a member of String
        [error]     println(new Optional(null).getOrElse(null))
        [error]                                         ^
        [error] one error found
    
    Even such common names as Some and None are not safe.
    One might think "who in their right mind would name their variables/classes
    using common names?!", but that definitely happens :)

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -2,6 +2,7 @@ class C { override def toString = "C" }
 
 object Test {
   def main(args: Array[String]): Unit = {
+    val Optional = "uh-oh"
     println(new Optional(null).getOrElse(null))
   }
 }
