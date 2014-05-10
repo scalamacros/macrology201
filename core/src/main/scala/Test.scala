4129f5151cebf07bca8d3e2c48fa6324aed9dbe9
Eugene Burmako <xeno.by@gmail.com>
2014-05-10 23:10:18 +0200

    step 18: allright, enough with name clashes. there's one more thing we haven't seen yet
    Let's roll back our `ensuringNotNull` changes, which will leave us with
    a trivially simple macro:
    
         q"""
           val $temp = $prefix
           if ($temp.isEmpty) $alt else $temp.value
         """
    
        def foo(x: Optional[C]) = x
        println(foo(new Optional(null)).getOrElse(new C))
    
    What if I told you that I can make a small change to the test shown above
    that is going to crash the compiler?!

--- core/src/main/scala/Test.scala
+++ core/src/main/scala/Test.scala
@@ -2,7 +2,7 @@ class C { override def toString = "C" }
 
 object Test {
   def main(args: Array[String]): Unit = {
-    val Optional = "uh-oh"
-    println(new Optional(null).getOrElse(null))
+    def foo(x: Optional[C]) = x
+    println(foo(new Optional(null)).getOrElse(new C))
   }
 }
--- macros/src/main/scala/Macros.scala
+++ macros/src/main/scala/Macros.scala
@@ -7,16 +7,8 @@ import scala.reflect.macros.blackbox.Context
 final class Optional[+A >: Null](val value: A) extends AnyVal {
   def get: A = value
   def isEmpty = value == null
-  // @inline final def getOrElse[B >: A](alt: => B): B = if (isEmpty) alt else value
   def getOrElse[B >: A](alt: => B): B = macro OptionalMacros.getOrElse
   override def toString = if (isEmpty) "<empty>" else s"$value"
-}
-
-object Optional {
-  def ensuringNotNull[A](x: A): A = {
-    if (x == null) sys.error("argument to Optional.getOrElse can't be null")
-    x
-  }
 }
 
 class OptionalMacros(val c: Context) {
@@ -24,10 +16,9 @@ class OptionalMacros(val c: Context) {
     import c.universe._
     val q"$prefix.$_[..$_](..$args)" = c.macroApplication
     val temp = c.freshName(TermName("temp"))
-    val Optional = q"_root_.Optional" // doesn't work!
     q"""
       val $temp = $prefix
-      if ($temp.isEmpty) $Optional.ensuringNotNull($alt) else $temp.value
+      if ($temp.isEmpty) $alt else $temp.value
     """
   }
 }
