744b6ab359796de27a87963644996266f35dfc33
Denys Shabalin <denys.shabalin@typesafe.com>
2014-05-12 13:17:56 +0200

    step 2: testing that the dummy materializer is wrong
    Even though it's clear that our dummy materializer is going to
    wrongly produce `Immutable` instances for any time, including
    mutable ones, we need a test to expose the wrong behavior so
    that our future versions can fix it for good.
    
    To test whether T is immutable we're adding a utility method called `is`
    that does a compile-time check for whether Immutable[T] is available
    and returns a runtime boolean.
    
    The `is` method is implemented as a simple macro that performs a search
    for an implicit value and returns true if it finds something and false
    if it doesn't. We don't actually need to explicitly branch here as we can
    just unquote boolean expression into a quasiquote and it will convert
    it to a tree for us (via lifting).
    
    Note how we create a type for Immutable[T], having only a type for T at hand.
    First of all, tq"..." isn't going to cut it, because it returns a Tree
    not a Type, and c.inferImplicitValue needs a Type. Secondly, Scala doesn't
    have quasiquotes for types yet, so we have to assemble the type by hand.
    In general this requires c.typecheck(..., c.TYPEmode), but for this particular
    case there's a shortcut.

--- src/main/scala/Immutable.scala
+++ src/main/scala/Immutable.scala
@@ -5,8 +5,16 @@ import scala.reflect.macros.whitebox.Context
 
 trait Immutable[T]
 object Immutable {
-  implicit def materialize[T]: Immutable[T] = macro impl[T]
-  def impl[T: c.WeakTypeTag](c: Context) = {
+  def is[T]: Boolean = macro isImpl[T]
+  def isImpl[T: c.WeakTypeTag](c: Context) = {
+    import c.universe._
+    val immutableOfT = appliedType(typeOf[Immutable[_]], weakTypeOf[T])
+    val inferred = c.inferImplicitValue(immutableOfT, silent = true)
+    q"${inferred.nonEmpty}"
+  }
+
+  implicit def materialize[T]: Immutable[T] = macro materializeImpl[T]
+  def materializeImpl[T: c.WeakTypeTag](c: Context) = {
     import c.universe._
     q"new Immutable[${weakTypeOf[T]}] { }"
   }
--- src/test/scala/ImmutableSuite.scala
+++ src/test/scala/ImmutableSuite.scala
@@ -2,5 +2,13 @@ import immutable.Immutable
 import org.scalatest.FunSuite
 
 class ImmutableSuite extends FunSuite {
+  test("dummy class is immutable") {
+    class Dummy
+    assert(Immutable.is[Dummy])
+  }
 
+  test("class with var is not immutable") {
+    class C(var x: Int)
+    assert(!Immutable.is[C])
+  }
 }
