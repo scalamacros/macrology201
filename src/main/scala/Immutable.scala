ac68508f087a4f604ed4821ff8e702c8da776b7f
Denys Shabalin <denys.shabalin@typesafe.com>
2014-05-12 12:58:57 +0200

    step 1: a dummy materializer
    As a zero-th approximation we provide evidence that every type is
    immutable. The resulting macro is very simple, but even it demonstrates
    a few important points:
    
    1. We choose whitebox context because we'd like the compiler to continue
    searching for implicit instances even if our macro fails. Usage of
    blackbox context would have implied that we can provide an
    evidence no matter what, so the failure of a macro expansion
    becomes a compilation error.
    
    2. Our type parameter needs a weak typetag so that we can reflect on it
    in a macro via weakTypeOf. Here we just use this type to create a dummy
    type class instance, but later on we'll be performing meaningful introspective
    operations on it.

--- src/main/scala/Immutable.scala
+++ src/main/scala/Immutable.scala
@@ -1,4 +1,13 @@
 package immutable
 
-trait Immutable[T]
+import language.experimental.macros
+import scala.reflect.macros.whitebox.Context
 
+trait Immutable[T]
+object Immutable {
+  implicit def materialize[T]: Immutable[T] = macro impl[T]
+  def impl[T: c.WeakTypeTag](c: Context) = {
+    import c.universe._
+    q"new Immutable[${weakTypeOf[T]}] { }"
+  }
+}
